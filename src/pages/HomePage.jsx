23524f4fc3cce4fe48dc85eea3a8b64b04cb6fcb
Lee TB <tblyduc2412@gmail.com>
2022-09-26 21:35:10 +0700

    style: format vietnam currency

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,60 +4,60 @@ import { Card } from '~/components';
 
 const data = [
   {
-    title: 'Mouse Bell',
-    price: { amount: 200000, unit: 'VND' },
-    rating: 3,
-    image:
-      'https://pawfriends.qodeinteractive.com/wp-content/uploads/2019/07/shop-img-2.jpg',
-  },
-  {
-    title: 'Rain coat',
-    price: { amount: 50000, unit: 'VND' },
-    rating: 4,
-    image:
-      'https://pawfriends.qodeinteractive.com/wp-content/uploads/2019/07/shop-img-15-1.jpg',
-  },
-  {
-    title: 'party hat',
-    price: { amount: 258000, unit: 'VND' },
-    rating: 2,
-    image:
-      'https://pawfriends.qodeinteractive.com/wp-content/uploads/2019/07/shop-img-4.jpg',
-  },
-  {
-    title: 'storage bag',
-    price: { amount: 58000, unit: 'VND' },
-    rating: 2,
-    image:
-      'https://pawfriends.qodeinteractive.com/wp-content/uploads/2019/07/shop-img-18-1.jpg',
-  },
-  {
     title: 'Đồ chơi bóng đồng giá',
-    price: { amount: 14000, unit: 'VND' },
+    price: { value: 14000, unit: 'VND' },
     rating: 2,
     image:
       'https://matpetfamily.com/wp-content/uploads/2021/01/kiotviet_eee77b1a2f3509ab4834d89d0748bf71-300x300.jpeg',
   },
   {
     title: 'Xịt vệ sinh đúng chỗ',
-    price: { amount: 55000, unit: 'VND' },
+    price: { value: 55000, unit: 'VND' },
     rating: 2,
     image:
-      'https://matpetfamily.com/wp-content/uploads/2021/0…viet_0940466be9c1ca03485eaaaf66e3506b-300x300.jpg',
+      'https://matpetfamily.com/wp-content/uploads/2021/05/kiotviet_0940466be9c1ca03485eaaaf66e3506b-300x300.jpg',
   },
   {
-    title: 'Đồ chơi bóng đồng giá',
-    price: { amount: 14000, unit: 'VND' },
+    title: 'Dây vòng cổ chuông',
+    price: { value: 14000, unit: 'VND' },
     rating: 2,
     image:
-      'https://matpetfamily.com/wp-content/uploads/2021/01/kiotviet_eee77b1a2f3509ab4834d89d0748bf71-300x300.jpeg',
+      'https://matpetfamily.com/wp-content/uploads/2021/05/kiotviet_a6359746e157b7d34d43f9d9ba408a0f.jpg',
   },
   {
     title: 'Vòng dây cổ chuông',
-    price: { amount: 50000, unit: 'VND' },
+    price: { value: 50000, unit: 'VND' },
     rating: 2,
     image:
-      'https://matpetfamily.com/wp-content/uploads/2021/01/kiotviet_eee77b1a2f3509ab4834d89d0748bf71-300x300.jpeg',
+      'https://matpetfamily.com/wp-content/uploads/2020/10/kiotviet_d15c1f7486eebcf8a252f0e5330e6e77-300x300.jpg',
+  },
+  {
+    title: 'Mouse Bell',
+    price: { value: 200000, unit: 'VND' },
+    rating: 3,
+    image:
+      'https://pawfriends.qodeinteractive.com/wp-content/uploads/2019/07/shop-img-2.jpg',
+  },
+  {
+    title: 'Rain coat',
+    price: { value: 50000, unit: 'VND' },
+    rating: 4,
+    image:
+      'https://pawfriends.qodeinteractive.com/wp-content/uploads/2019/07/shop-img-15-1.jpg',
+  },
+  {
+    title: 'party hat',
+    price: { value: 258000, unit: 'VND' },
+    rating: 2,
+    image:
+      'https://pawfriends.qodeinteractive.com/wp-content/uploads/2019/07/shop-img-4.jpg',
+  },
+  {
+    title: 'storage bag',
+    price: { value: 58000, unit: 'VND' },
+    rating: 2,
+    image:
+      'https://pawfriends.qodeinteractive.com/wp-content/uploads/2019/07/shop-img-18-1.jpg',
   },
 ];
 
