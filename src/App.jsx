69076bcef185a438789f16a763354ccca0843478
Lee TB <tblyduc2412@gmail.com>
2022-09-27 19:06:54 +0700

    feat: add product page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import MainLayout from '~/layouts/MainLayout';
 import Counter from './Counter';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="product/:productId" element={'san-pham'} />
+        <Route path="product/:productId" element={<ProductPage />} />
         <Route path="thanh-toan" element={'thanh-toan'} />
         <Route path="counter" element={<Counter />} />
       </Route>
