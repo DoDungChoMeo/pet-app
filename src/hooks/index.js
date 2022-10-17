0a134164ee9059530baf2107ba456ec5d4f189bf
Lee TB <tblyduc2412@gmail.com>
2022-10-17 08:53:46 +0700

    feat: add useFirestoreCollection hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,1 @@
+export { default as useFirestoreCollection } from './useFirestoreCollection/useFirestoreCollection';
---
+++ src/hooks/useFirestoreCollection/useFirestoreCollection.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getFirestore, onSnapshot, collection } from 'firebase/firestore';
+
+function useFirestoreCollection(collectionName) {
+  const [firestoreCollection, setFirestoreCollection] = useState([]);
+  const [firestoreLoading, setFirestoreLoading] = useState(true);
+
+  const firestore = getFirestore();
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(firestore, collectionName),
+      (snapshot) => {
+        setFirestoreCollection(snapshot.docs.map((e) => e.data()));
+        setFirestoreLoading(false);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return [firestoreCollection, firestoreLoading];
+}
+
+export default useFirestoreCollection;
