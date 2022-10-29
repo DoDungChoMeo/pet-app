837dc91d7f324fff74e5d0b4d1eabb6a5fa66308
Lee TB <tblyduc2412@gmail.com>
2022-10-29 10:06:55 +0700

    feat: add category, change layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,47 +1,81 @@
 import React from 'react';
-import { List } from 'antd';
-import { Card } from '~/components';
-// import { products } from '~/data';
+import { List, Row, Col, Skeleton } from 'antd';
 import styled from 'styled-components';
-import { useProductContext } from '~/contexts/ProductProvider';
-import { useFirestoreCollection } from '~/hooks';
+import { getFirestore, collection, query, where } from 'firebase/firestore';
+import { Card } from '~/components';
+import { Category } from '~/features/ProductFeature';
+import { useFirestoreCollection, useFirestoreQuery } from '~/hooks';
 
 function HomePage() {
-  const { products } = useProductContext();
-  const [inventories] = useFirestoreCollection('inventories');
+  const firestore = getFirestore();
+  const productQuery = query(
+    collection(firestore, 'products'),
+    where('status', '==', 'visible')
+  );
+  const [products, productsLoading] = useFirestoreQuery(productQuery);
+  const [categories, categoriesLoading] = useFirestoreCollection('categories');
+  const [brands, brandsLoading] = useFirestoreCollection('brands');
+
   return (
     <ContainerStyled className="home-page">
-      <List
-        grid={{
-          gutter: [20, 40],
-          xxl: 4,
-          xl: 4,
-          lg: 3,
-          md: 2,
-          sm: 1,
-          xs: 1,
-        }}
-        dataSource={products}
-        renderItem={(item) => {
-          const { title, images, rating, productId, bookmarkName } = item;
-          const inventory = inventories.find(
-            (inven) => inven.productId === productId
-          );
-
-          return (
-            <List.Item>
-              <Card
-                title={title}
-                image={images[0]}
-                rating={rating}
-                id={productId}
-                price={inventory?.price}
-                bookmarkName={bookmarkName}
+      <Row gutter={[20, 0]}>
+        <Col span={24} lg={4}>
+          <Category
+            title="Danh mục sản phẩm"
+            items={categories}
+            loading={categoriesLoading}
+          />
+          <Category title="Nhãn hiệu" items={brands} loading={brandsLoading} />
+        </Col>
+        <Col span={24} lg={20}>
+          {productsLoading ? (
+            <>
+              <Skeleton
+                active
+                paragraph={{
+                  rows: 8,
+                }}
               />
-            </List.Item>
-          );
-        }}
-      />
+            </>
+          ) : (
+            <List
+              grid={{
+                gutter: [20, 40],
+                xxl: 5,
+                xl: 5,
+                lg: 5,
+                md: 3,
+                sm: 2,
+                xs: 1,
+              }}
+              dataSource={products}
+              renderItem={(item) => {
+                const {
+                  title,
+                  images,
+                  rating,
+                  productId,
+                  bookmarkName,
+                  inventory,
+                } = item;
+
+                return (
+                  <List.Item>
+                    <Card
+                      title={title}
+                      image={images[0]}
+                      rating={rating}
+                      id={productId}
+                      price={inventory?.price}
+                      bookmarkName={bookmarkName}
+                    />
+                  </List.Item>
+                );
+              }}
+            />
+          )}
+        </Col>
+      </Row>
     </ContainerStyled>
   );
 }
