ce5cf9a04f622df9a2040513ef4a38aa64809f5a
Lee TB <tblyduc2412@gmail.com>
2022-11-13 19:43:26 +0700

    feat: sync cart to firestore database

--- src/contexts/CartProvider/CartProvider.jsx
+++ src/contexts/CartProvider/CartProvider.jsx
@@ -1,20 +1,33 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useContext, useEffect, useReducer, useState, useRef } from 'react';
 import cartReducer from './CartReducer';
 import {
+  SET_USER,
+  SET_CART,
   ADD_TO_CART,
   INCREASE_QUANTITY,
   DECREASE_QUANTITY,
   REMOVE_ITEM,
   INPUT_QUANTITY,
+  EXPIRED_DAY,
 } from './CartReducer';
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+} from 'firebase/firestore';
+import { getCookie, setCookie, checkCookie } from '~/utils/cookies';
 
 const CartContext = React.createContext();
 
 const initialState = {
-  cartId: '',
   userId: '',
-  status: '',
-  modifiedOn: new Date(),
+  status: 'pending',
+  modifiedDate: new Date(),
+  expirationDate: new Date(
+    new Date().setDate(new Date().getDate() + EXPIRED_DAY)
+  ),
   products: [],
   quantity: 0,
   total: 0,
@@ -22,6 +35,50 @@ const initialState = {
 
 function CartProvider({ children }) {
   const [cart, dispatch] = useReducer(cartReducer, initialState);
+  const [userId, setUserId] = useState();
+  const firestore = getFirestore();
+  console.log({ cart });
+
+  const handleCookie = (() => {
+    let executed = false;
+    return function () {
+      if (!executed) {
+        executed = true;
+        const userId = getCookie('user');
+        if (userId != '') {
+          return userId;
+        } else {
+          const userRef = doc(collection(firestore, 'users'));
+          setDoc(userRef, cart);
+          setCookie('user', userRef.id);
+        }
+      }
+    };
+  })();
+
+  // handle get - set cookie
+  useEffect(() => {
+    const data = handleCookie();
+    if (data) {
+      dispatch({ type: SET_USER, payload: { userId: data } });
+      setUserId(data);
+    }
+  }, []);
+
+  // fetch data from database and set cart
+  useEffect(() => {
+    if (userId) {
+      async function fetchData() {
+        const userRef = doc(firestore, `users/${userId}`);
+        const snap = await getDoc(userRef);
+        const data = snap.data();
+        if (data) {
+          dispatch({type: SET_CART, payload: {cart: data?.cart}})
+        }
+      }
+      fetchData();
+    }
+  }, [userId]);
 
   const addToCart = (product) => {
     dispatch({ type: ADD_TO_CART, payload: product });
